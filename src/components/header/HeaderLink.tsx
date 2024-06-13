790f67fa1f06a52ccef510dca78798c3a7c28c02
Nikolay Tarnavskii <nik.tarnavsky@gmail.com>
2024-06-13 18:41:57 +0300

    responsive in progress

--- src/components/header/HeaderLink.tsx
+++ src/components/header/HeaderLink.tsx
@@ -1,15 +1,16 @@
-import {Link} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./Header.module.scss";
 
-export default function HeaderLink({name, path}: {name: string, path: string}) {
+export default function HeaderLink({ name, path }: { name: string, path: string }) {
+    const location = useLocation();
     return (
-        <li>
-            <Link to={path} className={location.pathname === path
+        <Link
+            to={path}
+            className={location.pathname === path
                 ? `${styles.navLink_active}`
                 : `${styles.navLink}`}
-            >
-                {name}
-            </Link>
-        </li>
-    )
+        >
+            {name}
+        </Link>
+    );
 }
