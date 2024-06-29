d525fc7707dc2824adb958ce015f28057cce7ecc
Serhii Neliubov <sergeyneliubov@gmail.com>
2024-06-29 14:51:15 +0200

    fix(deploy): deleted comments and Pagination component

--- src/services/ProductsService.ts
+++ src/services/ProductsService.ts
@@ -1,52 +1,15 @@
-import { IProduct } from "@models/IProduct";
 import $api from "@utils/interceptors.ts";
-import { ICategory } from "@models/ICategory";
-
-
-const products : IProduct[] = [ 
-    {
-    id: '1',
-    title: 'Крем дял кожи',
-    brand_name: 'Nivea',
-    description: 'Super crem',
-    image: '',
-    upload_image: '',
-    avg_rating: 4,
-    total_reviews: 1,
-    sell_price: '2.02',
-    discount_price: '2',
-    clicks: 4,
-    website_from: 'http://google.com',
-    category_id: '1',
-    reviews: ['Mne ponravilos'],
-  }
- ]
-
-const categories : ICategory[] = [
-{
-    id: '1',
-    name: 'Crema'
-},
-{
-    id: '2',
-    name: 'Shampun'
-}
-]
-
-
-
 
 export class ProductsService {
     static async getProducts(size: number, page: number, category_id?: string[]) {
         try {
-            // const { data } = await $api.get(`/api/products`, {
-            //     params: {
-            //         size,
-            //         page,
-            //         category_id
-            //     }
-            // });
-            const data = {items:products};
+            const { data } = await $api.get(`/api/products`, {
+                params: {
+                    size,
+                    page,
+                    category_id
+                }
+            });
 
             return data;
         } catch (error) {
@@ -56,9 +19,7 @@ export class ProductsService {
 
     static async getProductById(id: string) {
         try {
-            // const { data } = await $api.get(`/api/products/${id}`);
-
-            const data = products.find((e) => e.id === id)
+            const { data } = await $api.get(`/api/products/${id}`);
 
             return data;
         } catch (error) {
@@ -68,14 +29,13 @@ export class ProductsService {
 
     static async getProductBySearch(value: string) {
         try {
-            // const { data } = await $api.get('/api/products/search', {
-            //     params: {
-            //         text: value,
-            //         page: 1,
-            //         size: 10
-            //     }
-            // });
-            const data = products.filter((e) =>e.title && e.title.includes(value) || e.description && e.description.includes(value) )
+            const { data } = await $api.get('/api/products/search', {
+                params: {
+                    text: value,
+                    page: 1,
+                    size: 10
+                }
+            });
 
             return data;
         } catch (error) {
@@ -86,8 +46,7 @@ export class ProductsService {
 
     static async getProductCategories() {
         try {
-            // const { data } = await $api.get('/api/category');
-            const data = categories;
+            const { data } = await $api.get('/api/category');
 
             return data;
         } catch (error) {
